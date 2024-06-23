190a52be49b2bf2a7bf6b4e60c76cc3cf4fcd1e1
Andreas Fehlner <fehlner@arcor.de>
2024-06-23 17:25:43 +0200

    update

--- tests/test_power_spherical.py
+++ tests/test_power_spherical.py
@@ -94,7 +94,30 @@ def test_dynamo_export_power_spherical_githubexample(tmp_path):
 
     exported_program = torch.export.export(PowerModel() , args=(torch.randn(1),))
     
+def test_dynamo_export_power_githubexample_onnx_static(tmp_path):
+    class PowerModel(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+            batch_size = 32
+            cloc = torch.randn(batch_size, 3)
+            cscale = torch.ones(batch_size)
+            self.power_spherical = PowerSpherical(loc=cloc, scale=cscale)
 
+        def forward(self, x):
+            return self.power_spherical.rsample()
+
+
+    exported_program = torch.export.export(PowerModel() , args=(torch.randn(1),))
+    x = torch.randn(2, 3)
+
+    #export_options = torch.onnx.ExportOptions(dynamic_shapes=True)
+
+    onnx_program = torch.onnx.dynamo_export(
+        exported_program,
+        x,
+        #export_options=export_options
+    )
+    onnx_program.save(str(tmp_path) + os.sep + "powerspherical_static.onnx")
 
 def test_dynamo_export_power_githubexample_onnx_dynamic_shapes(tmp_path):
     class PowerModel(torch.nn.Module):
