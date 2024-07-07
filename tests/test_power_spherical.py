e063f821b9a0ac40052b3162830898936e2c6445
Andreas Fehlner <fehlner@arcor.de>
2024-07-07 16:06:24 +0200

    multiple updates

--- tests/test_power_spherical.py
+++ tests/test_power_spherical.py
@@ -3,14 +3,14 @@
 #
 # SPDX-License-Identifier: MIT
 
+import os
+import random
 import sys
 
+import numpy as np
 import pytest
 import torch
 from power_spherical import HypersphericalUniform, PowerSpherical
-import numpy as np
-import random
-import os
 
 
 def test_power_spherical_2d():
@@ -62,60 +62,3 @@ def test_kl_divergence():
     )
 
 
-@pytest.mark.xfail(
-    reason="RuntimeError: Encountered autograd state manager op",
-)
-def test_dynamo_export_normal(tmp_path):
-    class Model(torch.nn.Module):
-        def __init__(self):
-            self.normal = torch.distributions.normal.Normal(0, 1)
-            super().__init__()
-
-        def forward(self, x):
-            return self.normal.sample(x.shape)
-
-    x = torch.randn(2, 3)
-    exported_program = torch.export.export(Model(), args=(x,))
-    onnx_program = torch.onnx.dynamo_export(
-        exported_program,
-        x,
-    )
-    onnx_program.save(str(tmp_path + os.sep + "normal.onnx"))
-
-
-@pytest.mark.xfail(reason="not supported feature of ONNX")
-def test_dynamo_export_spherical():
-    class Model(torch.nn.Module):
-        def __init__(self):
-            self.spherical = PowerSpherical(
-                torch.Tensor([0.0, 1.0]), torch.Tensor([1.0])
-            )
-            super().__init__()
-
-        def forward(self, x):
-            return self.spherical.sample(x.shape)
-
-    x = torch.randn(2, 3)
-    exported_program = torch.export.export(Model(), args=(x,))
-    _ = torch.onnx.dynamo_export(
-        exported_program,
-        x,
-    )
-
-# https://github.com/pytorch/pytorch/issues/116336
-@pytest.mark.xfail(reason="not supported feature of ONNX")
-def test_dynamo_export_power_spherical():
-    class PowerModel(torch.nn.Module):
-        def __init__(self):
-            super().__init__()
-            batch_size = 32
-            cloc = torch.randn(batch_size, 3)
-            cscale = torch.ones(batch_size)
-            self.power_spherical = PowerSpherical(loc=cloc, scale=cscale)
-
-        def forward(self, x):
-            return self.power_spherical.rsample()
-
-
-    x = torch.randn(1)
-    exported_program = torch.export.export(PowerModel() , args=(x,))
